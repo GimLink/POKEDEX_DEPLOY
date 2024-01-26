78cabcf98aa96f64075063d806dd37f51c207d50
Link <mexlove2@gmail.com>
2024-01-27 00:18:34 +0900

    포켓몬 레포지토리 기본 저장 및 업데이트 기능 추가

--- src/main/java/Link/pokemon/repository/pokemon/PokeRepository.java
+++ src/main/java/Link/pokemon/repository/pokemon/PokeRepository.java
@@ -1,10 +1,15 @@
 package Link.pokemon.repository.pokemon;
 
 import Link.pokemon.domain.pokemon.Pokemon;
+import Link.pokemon.domain.pokemon.PokemonUpdateDto;
 
 import java.util.Optional;
 
 public interface PokeRepository {
     Optional<Pokemon> findById(Long id);
+
+    Pokemon save(Pokemon pokemon);
+
+    void update(Long pokemonId, PokemonUpdateDto updateParam);
 
 }
--- src/main/java/Link/pokemon/repository/pokemon/PokemonRepository.java
+++ src/main/java/Link/pokemon/repository/pokemon/PokemonRepository.java
@@ -1,6 +1,7 @@
 package Link.pokemon.repository.pokemon;
 
 import Link.pokemon.domain.pokemon.Pokemon;
+import Link.pokemon.domain.pokemon.PokemonUpdateDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
@@ -19,4 +20,23 @@ public class PokemonRepository implements PokeRepository {
     @Override
     public Optional<Pokemon> findById(Long id) {
         return repository.findById(id);}
+
+    @Override
+    public Pokemon save(Pokemon pokemon) {
+        return repository.save(pokemon);
+    }
+
+    @Override
+    public void update(Long pokemonId, PokemonUpdateDto updateParam) {
+        Pokemon findPokemon = repository.findById(pokemonId).orElseThrow();
+
+        findPokemon.setIdPokemon(updateParam.getIdPokemon());
+        findPokemon.setPokemon(updateParam.getPokemon());
+        findPokemon.setHp(updateParam.getHp());
+        findPokemon.setAttack(updateParam.getAttack());
+        findPokemon.setDefense(updateParam.getDefense());
+        findPokemon.setSpecialAttack(updateParam.getSpecialAttack());
+        findPokemon.setSpecialDefense(updateParam.getSpecialDefense());
+        findPokemon.setSpeed(updateParam.getSpeed());
+    }
 }
