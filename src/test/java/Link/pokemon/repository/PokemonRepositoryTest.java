f8ce9c51087b9d13233ac7178692ecf348e2ca16
Link <mexlove2@gmail.com>
2024-01-29 19:54:15 +0900

    검색기능 테스트

--- src/test/java/Link/pokemon/repository/PokemonRepositoryTest.java
+++ src/test/java/Link/pokemon/repository/PokemonRepositoryTest.java
@@ -1,6 +1,7 @@
 package Link.pokemon.repository;
 
 import Link.pokemon.domain.pokemon.Pokemon;
+import Link.pokemon.domain.pokemon.PokemonSearchCond;
 import Link.pokemon.repository.pokemon.PokemonRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
@@ -8,6 +9,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Slf4j
 @Transactional
@@ -24,5 +27,14 @@ class PokemonRepositoryTest {
         log.info("pokemon = {}", pokemon);
 
         Assertions.assertThat(pokemon.getIdPokemon()).isEqualTo(1);
+    }
+
+    @Test
+    void findAll() {
+        PokemonSearchCond cond = new PokemonSearchCond(30, null, null, null,
+                null, null);
+        List<Pokemon> pokemon = repository.findAll(cond);
+
+        log.info("result = {}", pokemon);
     }
 }
