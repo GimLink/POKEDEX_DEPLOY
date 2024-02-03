b4dc05b9d0277ec6c87208c9b596fad95407a575
Link <mexlove2@gmail.com>
2024-02-03 19:48:12 +0900

    포켓몬, 타입 다대다 연결 테스트

--- src/test/java/Link/pokemon/repository/PokemonRepositoryTest.java
+++ src/test/java/Link/pokemon/repository/PokemonRepositoryTest.java
@@ -2,9 +2,13 @@ package Link.pokemon.repository;
 
 import Link.pokemon.domain.pokemon.Pokemon;
 import Link.pokemon.domain.pokemon.PokemonSearchCond;
+import Link.pokemon.domain.pokemon.Types;
+import Link.pokemon.repository.pokemon.PokemonQueryRepository;
 import Link.pokemon.repository.pokemon.PokemonRepository;
+import Link.pokemon.repository.pokemon.PokemonRepositoryV2;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
+import org.assertj.core.api.ListAssert;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,29 +16,41 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.*;
+
 @Slf4j
 @Transactional
 @SpringBootTest
 class PokemonRepositoryTest {
 
     @Autowired
-    PokemonRepository repository;
+    PokemonRepositoryV2 repository;
+    @Autowired
+    PokemonQueryRepository query;
 
     @Test
     void findById() {
 //        Pokemon testPokemon = new Pokemon(200L,"test", 1, 1, 1, 1, 1, 1);
         Pokemon pokemon = repository.findById(1L).get();
-        log.info("pokemon = {}", pokemon);
+        log.info("pokemon = {}", pokemon.getPokemon());
 
-        Assertions.assertThat(pokemon.getIdPokemon()).isEqualTo(1);
+        assertThat(pokemon.getIdPokemon()).isEqualTo(1);
     }
 
     @Test
     void findAll() {
         PokemonSearchCond cond = new PokemonSearchCond(30, null, null, null,
                 null, null);
-        List<Pokemon> pokemon = repository.findAll(cond);
+        List<Pokemon> pokemon = query.findAll(cond);
 
         log.info("result = {}", pokemon);
+    }
+
+    @Test
+    void findType() {
+        Pokemon pokemon = repository.findById(1L).get();
+        List<Types> types = pokemon.getTypes();
+        log.info("result = {}", types);
+
     }
 }
