77c5c31e29933d8fc90169b25db1dd00d1f75868
Link <mexlove2@gmail.com>
2024-02-10 23:47:32 +0900

    포켓몬 도감 목록에서 포켓몬 개별 상세 페이지 연결

--- src/main/java/Link/pokemon/web/PokemonController.java
+++ src/main/java/Link/pokemon/web/PokemonController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -24,5 +25,12 @@ public class PokemonController {
         List<Pokemon> pokemons = pokemonService.findAll(pokemonSearch);
         model.addAttribute("pokemons", pokemons);
         return "pokemons";
+    }
+
+    @GetMapping("/{idPokemon}")
+    public String pokemon(@PathVariable(name = "idPokemon") Long idPokemon, Model model) {
+        Pokemon pokemon = pokemonService.findById(idPokemon).get();
+        model.addAttribute("pokemon", pokemon);
+        return "pokemon";
     }
 }
