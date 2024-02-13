4878c6a8dbd71abf1b846a3988f23f0929f3b490
Link <mexlove2@gmail.com>
2024-02-13 20:42:23 +0900

    타입포켓몬 매핑

--- src/main/java/Link/pokemon/web/PokemonController.java
+++ src/main/java/Link/pokemon/web/PokemonController.java
@@ -2,7 +2,10 @@ package Link.pokemon.web;
 
 import Link.pokemon.domain.pokemon.Pokemon;
 import Link.pokemon.domain.pokemon.PokemonSearchCond;
+import Link.pokemon.domain.pokemon.Types;
 import Link.pokemon.service.pokemon.PokemonServiceV2;
+import Link.pokemon.service.type.TypeService;
+import Link.pokemon.service.type.TypeServiceInter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -19,6 +22,7 @@ import java.util.List;
 public class PokemonController {
 
     private final PokemonServiceV2 pokemonService;
+    private final TypeService typeService;
 
     @GetMapping
     public String pokemons(@ModelAttribute("pokemonSearch") PokemonSearchCond pokemonSearch, Model model) {
@@ -26,11 +30,16 @@ public class PokemonController {
         model.addAttribute("pokemons", pokemons);
         return "pokemons";
     }
-
     @GetMapping("/{idPokemon}")
     public String pokemon(@PathVariable(name = "idPokemon") Long idPokemon, Model model) {
         Pokemon pokemon = pokemonService.findById(idPokemon).get();
         model.addAttribute("pokemon", pokemon);
         return "pokemon";
+    }
+    @GetMapping("/type/{type}")
+    public String typePokemons(@PathVariable(name = "type") String typeName, Model model) {
+        Types type = typeService.findByType(typeName).get();
+        model.addAttribute("type", type);
+        return "typePokemons";
     }
 }
