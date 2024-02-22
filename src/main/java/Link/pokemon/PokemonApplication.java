b120709c6de6bcb00d0ff466a70bd13e6f89a794
Link <mexlove2@gmail.com>
2024-02-22 21:24:14 +0900

    회원 Config 설정

--- src/main/java/Link/pokemon/PokemonApplication.java
+++ src/main/java/Link/pokemon/PokemonApplication.java
@@ -1,13 +1,13 @@
 package Link.pokemon;
 
-import Link.pokemon.config.PokemonConfig;
-import Link.pokemon.config.PokemonConfigV2;
+import Link.pokemon.config.Member.MemberConfig;
+import Link.pokemon.config.Pokemon.PokemonConfigV2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
 //@Import((PokemonConfig.class))
-@Import(PokemonConfigV2.class)
+@Import({PokemonConfigV2.class, MemberConfig.class})
 @SpringBootApplication
 public class PokemonApplication {
 
