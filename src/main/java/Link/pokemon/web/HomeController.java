15cc423f1990bfa77ddadcb95694f883a64ffb86
Link <mexlove2@gmail.com>
2024-03-06 01:15:27 +0900

    로그인, 비로그인 홈 화면 차별화

--- src/main/java/Link/pokemon/web/HomeController.java
+++ src/main/java/Link/pokemon/web/HomeController.java
@@ -1,8 +1,12 @@
 package Link.pokemon.web;
 
+import Link.pokemon.domain.member.Member;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -11,8 +15,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 
     @GetMapping("/")
-    public String home() {
-        log.info("실행중");
-        return "home";
+    public String home(HttpServletRequest request, Model model) {
+
+        HttpSession session = request.getSession(false);
+
+        if (session == null) {
+            return "home";
+        }
+
+        Member loginMember = (Member) session.getAttribute("loginMember");
+
+        if (loginMember == null) {
+            return "home";
+        }
+
+        model.addAttribute("member", loginMember);
+
+        return "loginHome";
+
     }
 }
