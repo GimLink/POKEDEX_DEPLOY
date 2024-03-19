29e8f12015f6c3a3ba56268a4cec2d21ab38fdfd
Link <mexlove2@gmail.com>
2024-03-20 01:56:50 +0900

    BCryptPasswordEncoder 빈 등록

--- src/main/java/Link/pokemon/PokemonApplication.java
+++ src/main/java/Link/pokemon/PokemonApplication.java
@@ -4,12 +4,19 @@ import Link.pokemon.config.Member.MemberConfig;
 import Link.pokemon.config.Pokemon.PokemonConfigV2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 //@Import((PokemonConfig.class))
 @Import({PokemonConfigV2.class, MemberConfig.class})
 @SpringBootApplication
 public class PokemonApplication {
+
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(PokemonApplication.class, args);
