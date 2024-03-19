9e9bd0b07996b3af09e73ee9a830e812d770e2fd
Link <mexlove2@gmail.com>
2024-03-19 17:58:19 +0900

    더이상 인터셉터 사용 안하게끔 설정

--- src/main/java/Link/pokemon/config/web/WebConfig.java
+++ src/main/java/Link/pokemon/config/web/WebConfig.java
@@ -9,7 +9,7 @@ import jakarta.servlet.Filter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
+//@Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
